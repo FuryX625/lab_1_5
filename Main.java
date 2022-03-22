52f941176d1319ca99f037f96b138b05dddbfebd
serafin625 <serafin.m625@gmail.com>
2022-03-22 17:16:16 +0000

    całość

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   
+    for (int i = 50; i <= 100; i++) 
+    {
+      if(i%2!=0)
+      {
+        System.out.println(i);
+      }
+    }
+    
   }
 }
